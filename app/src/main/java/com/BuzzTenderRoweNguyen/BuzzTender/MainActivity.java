6668bfbe8a55617e614b0f7704045952f3d0315e
Christina Nguyen <63628857+christinan6220@users.noreply.github.com>
2021-10-19 16:44:52 -0400

    Updates mainactivity xml and started firebase auth - incomplete -Christina

--- app/src/main/java/com/BuzzTenderRoweNguyen/BuzzTender/MainActivity.java
+++ app/src/main/java/com/BuzzTenderRoweNguyen/BuzzTender/MainActivity.java
@@ -1,14 +1,92 @@
 package com.BuzzTenderRoweNguyen.BuzzTender;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth mAuth;
+
+    private ConstraintLayout mLoggedInGroup;
+    private ConstraintLayout mLoggedOutGroup;
+    private TextView mNameLabel;
+    private EditText mEmailField;
+    private EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoggedInGroup = findViewById(R.id.logged_in_group);
+        mLoggedOutGroup = findViewById(R.id.logged_out_group);
+        mNameLabel = findViewById(R.id.hello);
+        mEmailField = findViewById(R.id.email);
+        mPasswordField = findViewById(R.id.password);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    public void createAccount(View view) {
+        if (!validateForm()) {
+            return;
+        }
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If registration fails, display a message to the user.
+                            Exception e = task.getException();
+                            Log.w(TAG, "createUserWithEmail:failure", e);
+                            Toast.makeText(MainActivity.this, "Registration failed: " + e.getLocalizedMessage(),
+                                    Toast.LENGTH_LONG).show();
+                            updateUI(null);
+                        }
+                    }
+                });
     }
 }
