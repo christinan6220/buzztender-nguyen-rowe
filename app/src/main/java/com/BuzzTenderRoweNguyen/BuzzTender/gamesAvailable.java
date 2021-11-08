b11250742f20e7ac2fcc3d0dfb62b8f93c0b4391
Christina Nguyen <63628857+christinan6220@users.noreply.github.com>
2021-11-08 15:13:50 -0500

    started games_list_item.xml and there is also scratch code for the game adapter -Christina

--- app/src/main/java/com/BuzzTenderRoweNguyen/BuzzTender/gamesAvailable.java
+++ app/src/main/java/com/BuzzTenderRoweNguyen/BuzzTender/gamesAvailable.java
@@ -1,15 +1,22 @@
 package com.BuzzTenderRoweNguyen.BuzzTender;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class gamesAvailable extends AppCompatActivity {
@@ -18,6 +25,7 @@ public class gamesAvailable extends AppCompatActivity {
     private static final String TAG = "gamesAvailable";
     private static final String GAMES = "game";
     private ArrayAdapter<Games> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,24 @@ public class gamesAvailable extends AppCompatActivity {
         );
         gamesListView.setAdapter(adapter);
     }
+
+
+//    TODO: create gameadapter
+//    class GameAdapter extends ArrayAdapter<Games> {
+//        ArrayList<Games> games;
+//
+//        GameAdapter (Context context, ArrayList<Games> games) {
+//            super(context, 0 , games);
+//            this.games = games;
+//        }
+//        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//            if (convertView == null) {
+//                convertView = LayoutInflater.from(getContext()).inflate(R.layout.game_list_item, parent, false);      // reads the xml and converts into java widgets
+//            }
+//    }
+
+
+
     public void onLoadGamesClick(View view){
         mDb.collection(GAMES)
                 .get()
