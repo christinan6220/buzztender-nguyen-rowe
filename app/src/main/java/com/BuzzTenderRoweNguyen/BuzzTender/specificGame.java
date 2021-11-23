c110df0ec286833d19b3526d3396b3dfbc73e7b5
Christina Nguyen <63628857+christinan6220@users.noreply.github.com>
2021-11-23 01:16:50 -0500

    specificGame layout changes - now expandable cards -christina nguyen

--- app/src/main/java/com/BuzzTenderRoweNguyen/BuzzTender/specificGame.java
+++ app/src/main/java/com/BuzzTenderRoweNguyen/BuzzTender/specificGame.java
@@ -3,10 +3,20 @@ package com.BuzzTenderRoweNguyen.BuzzTender;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class specificGame extends AppCompatActivity {
+
+    private CardView descriptionCard;
+    private LinearLayout descriptionHidden, newGameCard;
+    private ImageButton descriptionArrow, newGameArrow;
+    private ConstraintLayout newGameHidden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,72 @@ public class specificGame extends AppCompatActivity {
         //create the up functionality in the toolbar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        descriptionCard = findViewById(R.id.game_description_card);
+        descriptionHidden = findViewById(R.id.description_hidden);
+        descriptionArrow = findViewById(R.id.description_expandable_button);
+        newGameCard = findViewById(R.id.new_game_visible);
+        newGameHidden = findViewById(R.id.lets_play_hidden);
+        newGameArrow = findViewById(R.id.new_game_arrow);
+
+
+//        SETTING UP CLICK LISTENERS
+//      Click listener for expanding the description card
+        descriptionCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (descriptionHidden.getVisibility() == View.VISIBLE){
+                    descriptionHidden.setVisibility(View.GONE);
+                    descriptionArrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
+                }
+                else {
+                    descriptionHidden.setVisibility(View.VISIBLE);
+                    descriptionArrow.setImageResource(R.drawable.ic_baseline_expand_less_24);
+                }
+            }
+        });
+        descriptionArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (descriptionHidden.getVisibility() == View.VISIBLE){
+                    descriptionHidden.setVisibility(View.GONE);
+                    descriptionArrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
+                }
+                else {
+                    descriptionHidden.setVisibility(View.VISIBLE);
+                    descriptionArrow.setImageResource(R.drawable.ic_baseline_expand_less_24);
+                }
+            }
+        });
+
+//      Click listener for expanding new game card
+        newGameCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (newGameHidden.getVisibility() == View.VISIBLE){
+                    newGameHidden.setVisibility(View.GONE);
+                    newGameArrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
+                }
+                else {
+                    newGameHidden.setVisibility(View.VISIBLE);
+                    newGameArrow.setImageResource(R.drawable.ic_baseline_expand_less_24);
+                }
+            }
+        });
+        newGameArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (newGameHidden.getVisibility() == View.VISIBLE){
+                    newGameHidden.setVisibility(View.GONE);
+                    newGameArrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
+                }
+                else {
+                    newGameHidden.setVisibility(View.VISIBLE);
+                    newGameArrow.setImageResource(R.drawable.ic_baseline_expand_less_24);
+                }
+            }
+        });
     }
+
+
 }
